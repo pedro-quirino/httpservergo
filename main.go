589cb8aff2bea5d2fa0acc0fed30fa509de701bb
Pedro Gomes <pedro.quirino@conductor.com.br>
2018-12-10 17:41:21 -0300

    Commit teste

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	pessoaProto "pessoa/pb"
+	"io/ioutil"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		//encode(r)
+		decode(r)
+
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
+
+func encode(r *http.Request) {
+
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// pessoa := &pb.Pessoa{}
+
+	// data, err := proto.Marshal(pessoa)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+}
+
+func decode(r *http.Request) {
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bytes := make([]byte, len(data))
+
+	p := &pb.pessoaProto{}
+
+	
+			if err := proto.Unmarshal(bytes, pessoa); err != nil {
+				fmt.Println(err)
+			}
+
+		print(p.GetNome())
+
+}
